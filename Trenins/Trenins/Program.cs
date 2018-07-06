847dc9809f44929dd17bbd13d19031df2037e2aa
Gincha <40606700+Gincha@users.noreply.github.com>
2018-07-06 14:49:42 +0300

    karatava

--- Trenins/Trenins/Program.cs
+++ Trenins/Trenins/Program.cs
@@ -6,11 +6,10 @@ namespace Trenins
     {
         static void Main(string[] args)
         {
-            Matematika darbibas = new Matematika();
-            Uzdevumi uzd = new Uzdevumi();
-            uzd.Izsauc();
+             Matematika darbibas = new Matematika();
+             Uzdevumi uzd = new Uzdevumi();
+             uzd.Izsauc();
 
-            Console.ReadLine();
         }
     }
 }
--- Trenins/Trenins/Uzdevumi.cs
+++ Trenins/Trenins/Uzdevumi.cs
@@ -8,7 +8,7 @@ namespace Trenins
     {
         public void Izsauc()
         {
-            Uzdevums27();
+            Uzdevums28();
         }
         private void Uzdevums1()
         {
@@ -524,8 +524,64 @@ namespace Trenins
             }
         }
 
+        private void Uzdevums28()
+        {
+
+            Console.WriteLine("Ievadi vardu");
+            string ievade = Console.ReadLine();
+            char[] vards = ievade.ToCharArray();
+            char[] svitras = ievade.ToCharArray();
+
+            for (int i = 0; i < vards.Length; i++)  //sadala minamo vardu pa burtiem
+            {
+                // Get character from array.
+                char burts = vards[i];
+            }
+
+            char stripa = '-';
+             //pec copy/paste vairs kludu nerada
+            for (int i = 0; i < vards.Length; i++)   // tads pats garums ka minamajam vardam 
+            {
+                svitras[i] = stripa;
+                char stripBurts = svitras[i];       
+
+            }
+
+            Console.WriteLine("Sakam");
+
+            int skaititajs =0;                  //minesim 10 reizes
+            do
+            {
+               skaititajs ++;
+                Console.WriteLine("Ievadi minejumu");
+                string ievade2 = Console.ReadLine();
+                char minejums = Convert.ToChar(ievade2);        //ievada burtu
+
+                string virkne = "";
+                string atmina = "";
+                for (int i = 0; i < vards.Length; i++)      //katram minejumam jaiet cauri visam arrayam ar minamo vardu
+                {
+                    char burts = vards[i];      //bez rindinas neatpazist mainigo if`a
+                    
+                    if (minejums == burts)          //ja minejums sakrit ar burtu minamaja varda
+                    {
+                        virkne = virkne + vards[i];     //ja sakrit burti tad parada burtu istaja vieta
+                    }
+                    else                                
+                    {
+                        virkne = virkne + svitras[i];       //ja nesakrit burti tad ieliek svitrinu burta vieta
+                    }
+                }
+
+                atmina = atmina + virkne;
+                Console.WriteLine(atmina);          //parada tikai 1 ciklu
+
+            } while (skaititajs != 10);     //iet cauri kamer nav 10 reizes
+
+            Console.WriteLine("Meginajumu skaits beidzies. Minamais vards bija - " + ievade);
+            Console.ReadLine();
+        }
+
     }
-
-
 }
 
